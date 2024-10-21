d4efa4c68a7cc50189310cfee51df2be9e270738
Abhishek Tripathi <Abhishek-72>
2024-10-21 21:01:03 +0530

    create  sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
+
 dotenv.config();
 
 mongoose
@@ -15,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000 !!");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
